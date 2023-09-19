14d804e07b17c3802ddcf72cab06fd278bf6fd1c
Intrum1 <skoropadskiy1979@gmail.com>
2023-09-19 16:27:44 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const saveCurrentTime = throttle(() => {
+    const iframe = document.getElementById('vimeo-player');
+    const player = new Player(iframe);
+
+    player.getCurrentTime().then(currentTime => {
+        localStorage.setItem('videoplayer-current-time', currentTime);
+    });
+}, 1000);
+
+function initializePlayer() {
+    const iframe = document.getElementById('vimeo-player');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', () => {
+        saveCurrentTime();
+    });
+
+    const savedTime = localStorage.getItem('videoplayer-current-time', '120');
+    if (savedTime !== null) {
+        player.setCurrentTime(savedTime).then(() => {
+            return;
+        });
+    }
+}
+initializePlayer();    
